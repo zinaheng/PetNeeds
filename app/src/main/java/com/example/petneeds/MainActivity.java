bd403bee58f98b99b5721ac685336a02d34cf4fb
sivakundana <sivakundana@gmail.com>
2021-04-16 22:34:44 -0500

    Added Google maps

--- app/src/main/java/com/example/petneeds/MainActivity.java
+++ app/src/main/java/com/example/petneeds/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.petneeds;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng Dallas = new LatLng(32.789444,96.791131);
+        map.addMarker(new MarkerOptions().position(Dallas).title("Dallas"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Dallas));
     }
 }
