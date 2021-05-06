d7b41fa152e83e5eff52132afa0ee16e65a6807d
andreariver <andrea.rivera2@mavs.uta.edu>
2021-05-06 11:06:47 -0500

    Search without zipcode

--- app/src/main/java/com/example/petneeds/MainActivity.java
+++ app/src/main/java/com/example/petneeds/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.petneeds;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.fragment.app.FragmentActivity;
 
-import android.location.Address;
-import android.location.Geocoder;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.SearchView;
-import android.widget.Spinner;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -24,7 +26,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,144 +36,46 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback,AdapterView.OnItemSelectedListener {
+import android.location.LocationListener;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback,AdapterView.OnItemSelectedListener,LocationListener {
 
 
     public static final String TAG = "MainActivity";
     static Double lng;
     static Double lat;
     static String name;
+    static String type;
+    static int count;
     List<PetInfo> info;
-    private Spinner sp;
     GoogleMap map;
     SupportMapFragment mapFragment;
     SearchView searchView;
+    LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        RecyclerView rvPetDetails = findViewById(R.id.rvPetDetails);
-//        final EditText enteredZip = (EditText) findViewById(R.id.etZipcode);
-//        ImageButton search = findViewById(R.id.searchbutton);
         searchView = findViewById(R.id.sv_location);
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//below is the code for setting up the drop down menu
-        sp = (Spinner) findViewById(R.id.drSpin);
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, items);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.Categories, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
-        sp.setAdapter(adapter);
 
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            },100);
+        }
 
-        sp.setOnItemSelectedListener(this);
-        //    @Override
-//    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//    }
-//
-//    @Override
-//    public void onNothingSelected(AdapterView<?> parent){
-//
-//    }
-//
-//}
-
-
-
-        info = new ArrayList<>();
-        final PetAdapter petAdapter = new PetAdapter(this, info);
-        rvPetDetails.setLayoutManager(new LinearLayoutManager(this));
-        rvPetDetails.setAdapter(petAdapter);
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                String location = searchView.getQuery().toString();
-                final String ZIPCODE = "https://maps.googleapis.com/maps/api/geocode/json?address=" + location + "&key=AIzaSyA3eIDiQk5MNmPvyx62qERmyb54UzORsIg";
-                /*List<Address> addressList = null;
-                if (location != null || !location.equals("")) {
-                    Geocoder geocoder = new Geocoder(MainActivity.this);
-                    try {
-                        addressList = geocoder.getFromLocationName(location, 1);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    Address address = addressList.get(0);
-                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-                    map.addMarker(new MarkerOptions().position(latLng).title(location));
-                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                }*/
-
-                final AsyncHttpClient client = new AsyncHttpClient();
-
-                client.get(ZIPCODE, new JsonHttpResponseHandler() {
-                    @Override
-                    public void onSuccess(int i, Headers headers, JSON json) {
-                        JSONObject jsonObject = json.jsonObject;
-                        try {
-                            lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
-                                    .getJSONObject("geometry").getJSONObject("location")
-                                    .getDouble("lng");
-
-                            lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0)
-                                    .getJSONObject("geometry").getJSONObject("location")
-                                    .getDouble("lat");
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        Log.i("Lat", String.valueOf(lat));
-                        final String PET_INFORMATION = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lng +
-                                "&radius=15000&type=pet_store&key=AIzaSyA3eIDiQk5MNmPvyx62qERmyb54UzORsIg";
-                        client.get(PET_INFORMATION, new JsonHttpResponseHandler() {
-                            public void onSuccess(int i, Headers headers, JSON json) {
-                                JSONObject jsonObject = json.jsonObject;
-                                try {
-                                    JSONArray results = jsonObject.getJSONArray("results");
-
-                                    for ( int j = 0 ; j <= 5 ; j++) {
-                                        lng = ((JSONArray) jsonObject.get("results")).getJSONObject(j)
-                                                .getJSONObject("geometry").getJSONObject("location")
-                                                .getDouble("lng");
-
-                                        lat = ((JSONArray) jsonObject.get("results")).getJSONObject(j)
-                                                .getJSONObject("geometry").getJSONObject("location")
-                                                .getDouble("lat");
-                                        name = ((JSONArray) jsonObject.get("results")).getJSONObject(j)
-                                                .getString("name");
-                                        LatLng latLng = new LatLng(lat, lng);
-                                        map.addMarker(new MarkerOptions().position(latLng).title(name));
-                                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
-                                    }
-
-
-                                    Log.i(TAG, "RESULTS:" + results.toString());
-                                    info.clear();
-                                    info.addAll(PetInfo.fromJsonArray(results));
-                                    petAdapter.notifyDataSetChanged();
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
-
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure(int i, Headers headers, String s, Throwable throwable) {
-
-                    }
-
-                });
-                return false;
+                type = searchView.getQuery().toString();
+                getLocation();
+                return true;
             }
 
             @Override
@@ -181,14 +84,108 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,MainActivity.this);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        try {
+            info = new ArrayList<>();
+
+            RecyclerView rvPetDetails = findViewById(R.id.rvPetDetails);
+            rvPetDetails.setLayoutManager(new LinearLayoutManager(this));
+            final PetAdapter petAdapter = new PetAdapter(this, info);
+            rvPetDetails.setAdapter(petAdapter);
+
+
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+
+            final AsyncHttpClient client = new AsyncHttpClient();
+
+            final String PET_INFORMATION = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + location.getLatitude() + "," + location.getLongitude() +
+                    "&radius=15000&name=" + type +"&key=AIzaSyA3eIDiQk5MNmPvyx62qERmyb54UzORsIg";
+
+            Log.i("URL: ", PET_INFORMATION);
+            client.get(PET_INFORMATION, new JsonHttpResponseHandler() {
+                public void onSuccess(int i, Headers headers, JSON json) {
+                    JSONObject jsonObject = json.jsonObject;
+                    try {
+                        JSONArray results = jsonObject.getJSONArray("results");
+
+                        int arlen = results.length();
+
+                        count = arlen < 5 ?  arlen : 5;
+                        map.clear();
+                        for (int j = 0; j < count; j++) {
+                            lng = ((JSONArray) jsonObject.get("results")).getJSONObject(j)
+                                    .getJSONObject("geometry").getJSONObject("location")
+                                    .getDouble("lng");
+
+                            lat = ((JSONArray) jsonObject.get("results")).getJSONObject(j)
+                                    .getJSONObject("geometry").getJSONObject("location")
+                                    .getDouble("lat");
+                            name = ((JSONArray) jsonObject.get("results")).getJSONObject(j)
+                                    .getString("name");
+                            LatLng latLng = new LatLng(lat, lng);
+                            map.addMarker(new MarkerOptions().position(latLng).title(name));
+                            map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        }
+
+
+                        Log.i(TAG, "RESULTS:" + results.toString());
+                        info.clear();
+                        info.addAll(PetInfo.fromJsonArray(results));
+                        petAdapter.notifyDataSetChanged();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                @Override
+                public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+
+                }
+            });
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         mapFragment.getMapAsync(this);
     }
 
     @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
     }
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         parent.getItemAtPosition(position);
